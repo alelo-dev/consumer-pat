96e38c1c1dda96b4745d2f13e3646dcf4a90b6c0
Almir Alves Pereira <almir.sne@gmail.com>
2021-06-27 21:41:35 -0300

    Remoção de função equals desnecessária, reformatação, AllArgsConstructor para os testes

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
@@ -1,42 +1,43 @@
 package br.com.alelo.consumer.consumerpat.entity;
 
-
-import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
-import java.util.Objects;
-
 
 @Data
 @Entity
+@AllArgsConstructor
 public class Consumer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Exclude
     Integer id;
+
     String name;
     int documentNumber;
     Date birthDate;
 
-    //contacts
+    // contacts
     int mobilePhoneNumber;
     int residencePhoneNumber;
     int phoneNumber;
     String email;
 
-    //Address
+    // Address
     String street;
     int number;
     String city;
     String country;
-    int portalCode;
+    int postalCode;
 
-    //cards
+    // cards
     int foodCardNumber;
     double foodCardBalance;
 
@@ -46,25 +47,5 @@ public class Consumer {
     int drugstoreNumber;
     double drugstoreCardBalance;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Consumer consumer = (Consumer) o;
-        return documentNumber == consumer.documentNumber
-                && mobilePhoneNumber == consumer.mobilePhoneNumber
-                && residencePhoneNumber == consumer.residencePhoneNumber
-                && phoneNumber == consumer.phoneNumber
-                && number == consumer.number
-                && portalCode == consumer.portalCode
-                && foodCardNumber == consumer.foodCardNumber
-                && Double.compare(consumer.foodCardBalance, foodCardBalance) == 0
-                && fuelCardNumber == consumer.fuelCardNumber && Double.compare(consumer.fuelCardBalance, fuelCardBalance) == 0
-                && drugstoreNumber == consumer.drugstoreNumber && Double.compare(consumer.drugstoreCardBalance, drugstoreCardBalance) == 0
-                && Objects.equals(id, consumer.id) && Objects.equals(name, consumer.name) && Objects.equals(birthDate, consumer.birthDate)
-                && Objects.equals(email, consumer.email) && Objects.equals(street, consumer.street) && Objects.equals(city, consumer.city)
-                && Objects.equals(country, consumer.country);
-    }
-
-
+    public Consumer() {}
 }
