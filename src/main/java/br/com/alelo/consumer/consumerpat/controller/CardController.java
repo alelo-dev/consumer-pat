14ab46003eb57798d6a6233661f0713444185405
Maico Rodrigo Caumo <maico.caumo@gmail.com>
2021-07-15 21:07:18 -0300

    documenting endpoints

--- src/main/java/br/com/alelo/consumer/consumerpat/controller/CardController.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/CardController.java
@@ -4,6 +4,8 @@ import br.com.alelo.consumer.consumerpat.dto.BuyDTO;
 import br.com.alelo.consumer.consumerpat.entity.Card;
 import br.com.alelo.consumer.consumerpat.service.CardService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -17,12 +19,23 @@ public class CardController extends BaseController<Card, CardService> {
     }
 
     @ApiOperation(value = "Update card balance")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Update balance successful"),
+            @ApiResponse(code = 400, message = "Balance less than zero"),
+            @ApiResponse(code = 404, message = "Card not found")}
+    )
     @PatchMapping(value = "/balance/{cardNumber}/{value}")
     public void updateBalance(@PathVariable long cardNumber, @PathVariable BigDecimal value) {
         getService().updateBalance(cardNumber, value);
     }
 
     @ApiOperation(value = "Make a purchase with the card")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful purchase"),
+            @ApiResponse(code = 400, message = "Card different from the type of establishment"),
+            @ApiResponse(code = 400, message = "Insufficient balance for operation"),
+            @ApiResponse(code = 404, message = "Card not found")}
+    )
     @PutMapping(value = "/buy")
     public void buy(@RequestBody BuyDTO buyDTO) {
         getService().buy(buyDTO);
