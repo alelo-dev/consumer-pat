5ba86e3c2204d1e55c8dfe888490a93368925b13
Brunno Silva <brunnoqsilva@gmail.com>
2021-08-23 22:28:47 -0300

    Versionamento da API para garantir retrocompatibilidade, ajustes documentaçao API, exibir http corretos.

--- src/main/java/br/com/alelo/consumer/consumerpat/config/SwaggerConfig.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package br.com.alelo.consumer.consumerpat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,9 +17,26 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.alelo.consumer.consumerpat.controller"))
-                .paths(PathSelectors.any())
-                .build();
+            .groupName("consumer-controller-1.0")
+            .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.alelo.consumer.consumerpat.controller.v1"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Consumer API").description(
+                        "Documentação de Consumer API v1.0, versão fora das boas práticas, mantida para compatibilidade de possíveis consumidores! " +
+                        "Altere a definição para obter a versão 2.0")
+                        .build());
+    }
+
+    @Bean
+    public Docket apiV2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .groupName("consumer-controller-2.0")
+            .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.alelo.consumer.consumerpat.controller.v2"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(new ApiInfoBuilder().version("2.0").title("Consumer API").description(
+                        "Documentação de Consumer API v2.0, construída com base nas práticas do Modelo de Maturidade Richardson.")
+                        .build());
     }
 }
--- src/main/java/br/com/alelo/consumer/consumerpat/controller/v2/ConsumerControllerV2.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/v2/ConsumerControllerV2.java
@@ -1,21 +1,29 @@
-package br.com.alelo.consumer.consumerpat.controller;
+package br.com.alelo.consumer.consumerpat.controller.v2;
 
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import br.com.alelo.consumer.consumerpat.entity.Extract;
 import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
 import br.com.alelo.consumer.consumerpat.respository.ExtractRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.List;
 
-
-@Controller
-@RequestMapping("/consumer")
-public class ConsumerController {
+@RestController
+@RequestMapping("/v2/consumer")
+public class ConsumerControllerV2 {
 
     @Autowired
     ConsumerRepository repository;
@@ -23,61 +31,78 @@ public class ConsumerController {
     @Autowired
     ExtractRepository extractRepository;
 
-
     /* Deve listar todos os clientes (cerca de 500) */
-    @ResponseBody
-    @ResponseStatus(code = HttpStatus.OK)
-    @RequestMapping(value = "/consumerList", method = RequestMethod.GET)
-    public List<Consumer> listAllConsumers() {
-        return repository.getAllConsumersList();
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Deve Listar todos os clientes", code = 200)
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Nenhum cliente encontrado!"),
+        @ApiResponse(code = 500, message = "Erro não tratado pelo servidor.")
+    })
+    public ResponseEntity<List<Consumer>> listAllConsumers() {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.getAllConsumersList());
     }
 
-
     /* Cadastrar novos clientes */
-    @RequestMapping(value = "/createConsumer", method = RequestMethod.POST)
-    public void createConsumer(@RequestBody Consumer consumer) {
-        repository.save(consumer);
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Deve Listar todos os clientes", code = 200)
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Nenhum cliente encontrado!"),
+        @ApiResponse(code = 500, message = "Erro não tratado pelo servidor.")
+    })
+    public ResponseEntity<Consumer> createConsumer(@RequestBody Consumer consumer) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(consumer));
     }
 
     // Não deve ser possível alterar o saldo do cartão
-    @RequestMapping(value = "/updateConsumer", method = RequestMethod.POST)
-    public void updateConsumer(@RequestBody Consumer consumer) {
-        repository.save(consumer);
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Nenhum cliente encontrado!"),
+        @ApiResponse(code = 500, message = "Erro não tratado pelo servidor.")
+    })
+    public ResponseEntity<Consumer> updateConsumer(@RequestBody Consumer consumer) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(consumer));
     }
-
 
     /*
      * Deve creditar(adicionar) um valor(value) em um no cartão.
      * Para isso ele precisa indenficar qual o cartão correto a ser recarregado,
      * para isso deve usar o número do cartão(cardNumber) fornecido.
      */
-    @RequestMapping(value = "/setcardbalance", method = RequestMethod.GET)
-    public void setBalance(int cardNumber, double value) {
+    @PutMapping(path = "/cardbalance", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Nenhum cliente encontrado!"),
+        @ApiResponse(code = 500, message = "Erro não tratado pelo servidor.")
+    })
+    public ResponseEntity<Consumer> updateBalance(int cardNumber, double value) {
         Consumer consumer = null;
         consumer = repository.findByDrugstoreNumber(cardNumber);
 
         if(consumer != null) {
             // é cartão de farmácia
             consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() + value);
-            repository.save(consumer);
+            return ResponseEntity.status(HttpStatus.OK).body(repository.save(consumer));
         } else {
             consumer = repository.findByFoodCardNumber(cardNumber);
             if(consumer != null) {
                 // é cartão de refeição
                 consumer.setFoodCardBalance(consumer.getFoodCardBalance() + value);
-                repository.save(consumer);
+                return ResponseEntity.status(HttpStatus.OK).body(repository.save(consumer));
             } else {
                 // É cartão de combustivel
                 consumer = repository.findByFuelCardNumber(cardNumber);
                 consumer.setFuelCardBalance(consumer.getFuelCardBalance() + value);
-                repository.save(consumer);
+                return ResponseEntity.status(HttpStatus.OK).body(repository.save(consumer));
             }
         }
     }
 
     @ResponseBody
-    @RequestMapping(value = "/buy", method = RequestMethod.GET)
-    public void buy(int establishmentType, String establishmentName, int cardNumber, String productDescription, double value) {
+    @PostMapping(path = "/buy" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Nenhum cliente encontrado!"),
+        @ApiResponse(code = 500, message = "Erro não tratado pelo servidor.")
+    })
+    public ResponseEntity<Extract> buy(int establishmentType, String establishmentName, int cardNumber, String productDescription, double value) {
         Consumer consumer = null;
         /* O valores só podem ser debitados dos cartões com os tipos correspondentes ao tipo do estabelecimento da compra.
         *  Exemplo: Se a compra é em um estabelecimeto de Alimentação(food) então o valor só pode ser debitado do cartão e alimentação
@@ -113,7 +138,7 @@ public class ConsumerController {
         }
 
         Extract extract = new Extract(establishmentName, productDescription, new Date(), cardNumber, value);
-        extractRepository.save(extract);
+        return ResponseEntity.status(HttpStatus.OK).body(extractRepository.save(extract));
     }
 
 }
