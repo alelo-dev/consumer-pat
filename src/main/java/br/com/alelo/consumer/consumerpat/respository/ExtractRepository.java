96a9f59ca5c8e5be8312cffeeda4b922eaf613c3
Caio Gonzales <csgonzales1745@gmail.com>
2021-08-30 13:04:51 -0300

    refactor: adjust id type to Long

--- src/main/java/br/com/alelo/consumer/consumerpat/respository/ExtractRepository.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/ExtractRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alelo.consumer.consumerpat.domain.entity.Extract;
 
-public interface ExtractRepository extends JpaRepository<Extract, Integer> {
+public interface ExtractRepository extends JpaRepository<Extract, Long> {
 }
