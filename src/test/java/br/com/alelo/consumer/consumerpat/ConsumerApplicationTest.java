f40878685c5313f6223547f6b697c0a707a1d076
Guilherme de Castro Fernandes <guilherme.cfernandes@original.com.br>
2021-09-01 14:05:01 -0300

    Renamed Controller and Test Class

--- src/test/java/br/com/alelo/consumer/consumerpat/ConsumerApplicationTest.java
+++ src/test/java/br/com/alelo/consumer/consumerpat/ConsumerApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConsumerTestApplicationTests {
+class ConsumerApplicationTest {
 
 	@Test
 	void contextLoads() {
