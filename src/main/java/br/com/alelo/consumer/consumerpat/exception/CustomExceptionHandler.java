a625e91926c101187b0092a1775eaf25c55b24de
Thiago Silveira <thiago.psilveira@aluno.faculdadeimpacta.com.br>
2021-09-22 10:14:39 -0300

    Teste Revisao Codigo Alelo

---
+++ src/main/java/br/com/alelo/consumer/consumerpat/exception/CustomExceptionHandler.java
@@ -0,0 +1,4 @@
+package br.com.alelo.consumer.consumerpat.exception;
+
+public class CustomExceptionHandler {
+}
---
+++ src/main/java/br/com/alelo/consumer/consumerpat/utils/DataUtils.java
@@ -0,0 +1,4 @@
+package br.com.alelo.consumer.consumerpat.utils;
+
+public class DataUtils {
+}
