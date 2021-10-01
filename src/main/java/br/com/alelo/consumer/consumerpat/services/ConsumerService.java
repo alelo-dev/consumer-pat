3b215fbd63add423f9772d0f9471a7546f7e98af
Valdir Cezar <valdircezar312@gmail.com>
2021-10-01 19:17:18 -0300

    camada de servicos para consumer iniciada

---
+++ src/main/java/br/com/alelo/consumer/consumerpat/services/ConsumerService.java
@@ -0,0 +1,24 @@
+package br.com.alelo.consumer.consumerpat.services;
+
+import br.com.alelo.consumer.consumerpat.entity.Consumer;
+import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ConsumerService {
+
+    @Autowired
+    private ConsumerRepository repository;
+
+    /**
+     * Método que busca um consumer por id
+     */
+    public Consumer findById(Integer id) {
+        Optional<Consumer> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
