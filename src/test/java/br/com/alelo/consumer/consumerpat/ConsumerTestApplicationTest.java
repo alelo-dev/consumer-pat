06d05cace053d0c682e3126a74f9da962a123e55
Valdir Cezar <valdircezar312@gmail.com>
2021-10-05 19:54:24 -0300

    testes para classe principal

---
+++ src/test/java/br/com/alelo/consumer/consumerpat/ConsumerTestApplicationTest.java
@@ -0,0 +1,15 @@
+package br.com.alelo.consumer.consumerpat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConsumerTestApplicationTest {
+
+    @Test
+    void main() {
+        ConsumerTestApplication.main(new String[] {});
+    }
+}
