48db0549295cebb40612844016bc7f3e11521f84
Valdir Cezar <valdircezar312@gmail.com>
2021-10-05 22:17:12 -0300

    ajuste swagger config

--- src/main/java/br/com/alelo/consumer/consumerpat/config/SwaggerConfig.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/config/SwaggerConfig.java
@@ -15,11 +15,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String TITLE        = "Teste Code Review";
+    private static final String DESCRIPTION  = "Um teste para vaga Dev Pleno Alelo";
+    private static final String VERSION      = "1.0.0";
+    private static final String LICENSE      = "Apache License Version 2.0";
+    private static final String LICENSE_URL  = "https://www.apache.org/licenses/LICENSE-2.0";
+    private static final String NAME_AUTHOR  = "Valdir Cezar";
+    private static final String SITE_AUTHO   = "https://www.linkedin.com/in/valdircezar/";
+    private static final String EMAIL_AUTHOR = "valdircezar312@gmail.com";
+    private static final String BASE_PACKAGE = "br.com.alelo.consumer.consumerpat";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.alelo.consumer.consumerpat"))
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false)
@@ -28,15 +38,12 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Teste Code Review")
-                .description("Um teste para vaga Dev Pleno Alelo")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                        .contact(new Contact(
-                                "Valdir Cezar",
-                                "https://www.linkedin.com/in/valdircezar/",
-                                "valdircezar312@gmail.com"))
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version(VERSION)
+                .license(LICENSE)
+                .licenseUrl(LICENSE_URL)
+                        .contact(new Contact(NAME_AUTHOR, SITE_AUTHO, EMAIL_AUTHOR))
                         .build();
     }
 
