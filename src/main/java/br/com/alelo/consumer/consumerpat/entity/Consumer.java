c9803d0a91d5174843c3d51608ddf36b5edca35d
demoniodehavaianas <nil.obermuller@gmail.com>
2022-01-16 00:01:24 -0300

    adicionando acessores, mapeamentos e relacionamentos

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
@@ -1,50 +1,65 @@
 package br.com.alelo.consumer.consumerpat.entity;
 
 
-import jdk.jfr.DataAmount;
-import lombok.Data;
+import br.com.alelo.consumer.consumerpat.dto.ConsumerInDTO;
+import br.com.alelo.consumer.consumerpat.enums.CardType;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Consumer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
-    String name;
-    int documentNumber;
-    Date birthDate;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String documentNumber;
+    private LocalDate birthDate;
+    private String email;
 
-    //contacts
-    int mobilePhoneNumber;
-    int residencePhoneNumber;
-    int phoneNumber;
-    String email;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "phone_id")
+    private Phone phone;
 
-    //Address
-    String street;
-    int number;
-    String city;
-    String country;
-    int portalCode;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "address_id")
+    private Address address;
 
     //cards
-    int foodCardNumber;
-    double foodCardBalance;
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private Set<Card> cards;
 
-    int fuelCardNumber;
-    double fuelCardBalance;
+    public Consumer() {
+    }
 
-    int drugstoreNumber;
-    double drugstoreCardBalance;
+    public Consumer(ConsumerInDTO dto) {
+
+        this.name = dto.getName();
+        this.birthDate = dto.getBirthDate();
+        this.documentNumber = dto.getDocumentNumber();
+        this.email = dto.getEmail();
+        this.phone = new Phone(dto.getPhone());
+        this.address = new Address(dto.getAddress());
+        this.cards = (cards == null) ? new HashSet<>() : cards;
+        dto.getCards().stream().forEach(c -> {
+            if (c.getCardType().compareTo(CardType.DRUGSTORE_CARD) == 0)
+                this.cards.add(new DrugstoreCard(c));
+            if (c.getCardType().compareTo(CardType.FOOD_CARD) == 0)
+                this.cards.add(new FoodCard(c));
+            if (c.getCardType().compareTo(CardType.FUEL_CARD) == 0)
+                this.cards.add(new FuelCard(c));
+        });
+//        this.cards = new Card(dto.getCards());
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -52,19 +67,10 @@ public class Consumer {
         if (o == null || getClass() != o.getClass()) return false;
         Consumer consumer = (Consumer) o;
         return documentNumber == consumer.documentNumber
-                && mobilePhoneNumber == consumer.mobilePhoneNumber
-                && residencePhoneNumber == consumer.residencePhoneNumber
-                && phoneNumber == consumer.phoneNumber
-                && number == consumer.number
-                && portalCode == consumer.portalCode
-                && foodCardNumber == consumer.foodCardNumber
-                && Double.compare(consumer.foodCardBalance, foodCardBalance) == 0
-                && fuelCardNumber == consumer.fuelCardNumber && Double.compare(consumer.fuelCardBalance, fuelCardBalance) == 0
-                && drugstoreNumber == consumer.drugstoreNumber && Double.compare(consumer.drugstoreCardBalance, drugstoreCardBalance) == 0
-                && Objects.equals(id, consumer.id) && Objects.equals(name, consumer.name) && Objects.equals(birthDate, consumer.birthDate)
-                && Objects.equals(email, consumer.email) && Objects.equals(street, consumer.street) && Objects.equals(city, consumer.city)
-                && Objects.equals(country, consumer.country);
+                && Objects.equals(id, consumer.id)
+                && Objects.equals(name, consumer.name)
+                && Objects.equals(birthDate, consumer.birthDate)
+                && Objects.equals(email, consumer.email)
+                && Objects.equals(address, consumer.address);
     }
-
-
 }
