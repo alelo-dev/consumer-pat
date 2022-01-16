994253bf20684ca96898d947fb5bd57ff09516d5
demoniodehavaianas <nil.obermuller@gmail.com>
2022-01-16 00:08:33 -0300

    add paginação ao recuperar os Consumers

--- src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
@@ -1,22 +1,27 @@
 package br.com.alelo.consumer.consumerpat.respository;
 
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
+public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
 
     @Query(nativeQuery = true, value = "select * from Consumer")
-    List<Consumer> getAllConsumersList();
+    Page<Consumer> getAllConsumersList(Pageable pageable);
+
+    @Query(nativeQuery = true, value = "selet * from Card where number = ? and cardType = ?")
+    Consumer findByCardNumber(String cardNumber, String cardType);
 
     @Query(nativeQuery = true, value = "select * from Consumer where FOOD_CARD_NUMBER = ? ")
-    Consumer findByFoodCardNumber(int cardNumber);
+    Consumer findByFoodCardNumber(String cardNumber);
 
     @Query(nativeQuery = true, value = "select * from Consumer where FUEL_CARD_NUMBER = ? ")
-    Consumer findByFuelCardNumber(int cardNumber);
+    Consumer findByFuelCardNumber(String cardNumber);
 
     @Query(nativeQuery = true, value = "select * from Consumer where DRUGSTORE_NUMBER = ? ")
-    Consumer findByDrugstoreNumber(int cardNumber);
+    Consumer findByDrugstoreNumber(String cardNumber);
 }
