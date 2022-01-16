985fd811912f27a4e9e96fef1cda457755550fc8
demoniodehavaianas <nil.obermuller@gmail.com>
2022-01-16 00:00:04 -0300

    adicioando colunas novas, mapeamentos, definições de tamanho.

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Card.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Card.java
@@ -1,11 +1,16 @@
 package br.com.alelo.consumer.consumerpat.entity;
 
+import br.com.alelo.consumer.consumerpat.enums.CardType;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
         property = "cardType")
@@ -15,7 +20,7 @@ import javax.persistence.*;
         @JsonSubTypes.Type(value = FuelCard.class, name = "FUEL_CARD")})
 @Getter
 @Setter
-@Entity
+@Entity(name = "cards")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "cardType", discriminatorType = DiscriminatorType.STRING)
 public abstract class Card {
@@ -24,11 +29,24 @@ public abstract class Card {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "balance", columnDefinition = "Decimal(10,2)")
+    @ColumnDefault("0")
+    @Setter(value = AccessLevel.PROTECTED)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Column(columnDefinition = "varchar(16)")
     private String number;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "cardType", insertable = false, updatable = false)
     private CardType cardType;
 
-    public abstract double cardBalance();
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<Extract> extract;
+
+    public void addBalance(final BigDecimal balance) {
+        this.balance = this.balance.add(balance);
+    }
+
+    public abstract void subtractBalance(final BigDecimal balance);
 }
