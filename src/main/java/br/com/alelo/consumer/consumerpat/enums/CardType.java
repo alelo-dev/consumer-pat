3b73981b7145b92b6cd3703b940aeb2e0c1654ac
demoniodehavaianas <nil.obermuller@gmail.com>
2022-01-15 23:58:59 -0300

    Criando Enums, movendo para nova organização;

--- src/main/java/br/com/alelo/consumer/consumerpat/enums/CardType.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/enums/CardType.java
@@ -1,4 +1,4 @@
-package br.com.alelo.consumer.consumerpat.entity;
+package br.com.alelo.consumer.consumerpat.enums;
 
 public enum CardType {
 
---
+++ src/main/java/br/com/alelo/consumer/consumerpat/enums/EstablishmentType.java
@@ -0,0 +1,6 @@
+package br.com.alelo.consumer.consumerpat.enums;
+
+public enum EstablishmentType {
+
+    FOOD, DRUGSTORE, FUEL;
+}
