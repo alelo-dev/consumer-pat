42c2437f340179770df64d718d67c370ae552726
jeffersoncarlospedroso <jeffersoncarlospedroso@gmail.com>
2022-01-20 16:55:03 -0300

    ajustado nome pacote repository

--- src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
@@ -2,8 +2,8 @@ package br.com.alelo.consumer.consumerpat.controller;
 
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import br.com.alelo.consumer.consumerpat.entity.Extract;
-import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
-import br.com.alelo.consumer.consumerpat.respository.ExtractRepository;
+import br.com.alelo.consumer.consumerpat.repository.ConsumerRepository;
+import br.com.alelo.consumer.consumerpat.repository.ExtractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
