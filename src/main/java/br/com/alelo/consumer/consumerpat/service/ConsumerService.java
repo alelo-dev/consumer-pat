457da0f3b328a391bf45191e254dd5e7ea9130fb
jeffersoncarlospedroso <jeffersoncarlospedroso@gmail.com>
2022-01-22 13:45:44 -0300

    adicionado doc metodos

--- src/main/java/br/com/alelo/consumer/consumerpat/service/ConsumerService.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/service/ConsumerService.java
@@ -8,10 +8,26 @@ import org.springframework.data.domain.Pageable;
 
 public interface ConsumerService {
 
+    /**
+     * Salva um consumidor
+     * @param consumer - Consumidor
+     * @return
+     */
     Consumer createConsumer(final Consumer consumer);
 
+    /**
+     * Atualiza os dados do consumidor
+     * @param id - ID do consumidor
+     * @param consumer - DTO de atualização do consumidor
+     * @return
+     */
     Consumer updateConsumer(final Integer id, final UpdateConsumerDTO consumer);
 
+    /**
+     * Retorna a lista de consumidores paginada
+     * @param pageable
+     * @return
+     */
     Page<ResponseConsumerDTO> getPageConsumer(final Pageable pageable);
 
 }
