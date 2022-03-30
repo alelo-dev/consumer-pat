664bcd53db19a403b3d6769207ad09955be8b422
Vinicius Guedes Lirio <vinicius.lirio@gsoftware.com.br>
2022-03-29 23:08:32 -0300

    fix data.sql to h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- TIPOS DE Cartao
+insert into card_type (id, name) values (1, 'Food');
+insert into card_type (id, name) values (2, 'Drugstore');
+insert into card_type (id, name) values (3, 'Fuel');
