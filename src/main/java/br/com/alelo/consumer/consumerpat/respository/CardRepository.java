a42bfb75362fcfd5771eadb86522b1fc94f08c0c
Vinicius Guedes Lirio <vinicius.lirio@gsoftware.com.br>
2022-03-29 23:07:18 -0300

    created repositories

---
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/CardRepository.java
@@ -0,0 +1,14 @@
+package br.com.alelo.consumer.consumerpat.respository;
+
+import br.com.alelo.consumer.consumerpat.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CardRepository extends JpaRepository<Card, Integer> {
+
+    @Query(nativeQuery = true, value = "select * from Card where number = :cardNumber ")
+    Optional<Card> findByNumber(Long cardNumber);
+
+}
