05e8ab15f55c2a61583baa12bdc7f8e62e36d32d
Diego Souza <diegosouza.pw@outlook.com>
2022-04-11 00:15:03 -0300

    Correction to work with uuid with 5ff60e5d12 and 5ff-60e-5d12c

--- src/main/java/br/com/alelo/consumer/consumerpat/utils/UUIDUtils.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/utils/UUIDUtils.java
@@ -9,6 +9,11 @@ import lombok.Data;
 public class UUIDUtils {
 
 	public static UUID makeUuid(String uuidString) {
+		
+		if(uuidString.contains("-")) {
+			return UUID.fromString(uuidString);
+		}
+
 		UUID uuid = new UUID(
 				new BigInteger(uuidString.substring(0, 16), 16).longValue(),
 				new BigInteger(uuidString.substring(16), 16).longValue());
