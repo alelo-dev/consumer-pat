4e41ae4fd6aec623d29f1667b704e9c45e958e06
Renan Ravelli <renan.fcamara@fornecedores.klabin.com.br>
2022-07-13 18:48:29 -0300

    Adicionando metodo para buscar um cartão pelo numero

--- src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
@@ -1,22 +1,12 @@
 package br.com.alelo.consumer.consumerpat.respository;
 
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
 
-    @Query(nativeQuery = true, value = "select * from Consumer")
-    List<Consumer> getAllConsumersList();
-
-    @Query(nativeQuery = true, value = "select * from Consumer where FOOD_CARD_NUMBER = ? ")
-    Consumer findByFoodCardNumber(int cardNumber);
-
-    @Query(nativeQuery = true, value = "select * from Consumer where FUEL_CARD_NUMBER = ? ")
-    Consumer findByFuelCardNumber(int cardNumber);
-
-    @Query(nativeQuery = true, value = "select * from Consumer where DRUGSTORE_NUMBER = ? ")
-    Consumer findByDrugstoreNumber(int cardNumber);
+  @Query(value = "select a from Consumer a where a.drugstoreNumber = :number or a.foodCardNumber = :number or a.fuelCardNumber = :number")
+  Optional<Consumer> findByCardNumber(int number);
 }
