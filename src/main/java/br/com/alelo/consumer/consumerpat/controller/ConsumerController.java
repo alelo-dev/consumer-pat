0fc458be600c3d1f59d6ff4138bcdf5ace7426f0
Jane Pellim <jane.pellim.e@riachuelo.com.br>
2022-07-17 17:30:17 -0300

    retirada dos comentarios

--- src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
@@ -60,43 +60,20 @@ public class ConsumerController {
      * Para isso ele precisa indenficar qual o cartão correto a ser recarregado,
      * para isso deve usar o número do cartão(cardNumber) fornecido.
      */
-    @RequestMapping(value = "/setcardbalance", method = RequestMethod.GET)
+    @RequestMapping(value = "/setcardbalance", method = RequestMethod.PUT)
     @ResponseStatus(code = HttpStatus.OK)
     public void setBalance(int cardNumber, double value) {
 
         cardService.setCardBalance(cardNumber, value);
-        /*
-        Consumer consumer = null;
-        consumer = repository.findByDrugstoreNumber(cardNumber);
 
-        if(consumer != null) {
-            // é cartão de farmácia
-            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() + value);
-            repository.save(consumer);
-        } else {
-            consumer = repository.findByFoodCardNumber(cardNumber);
-            if(consumer != null) {
-                // é cartão de refeição
-                consumer.setFoodCardBalance(consumer.getFoodCardBalance() + value);
-                repository.save(consumer);
-            } else {
-                // É cartão de combustivel
-                consumer = repository.findByFuelCardNumber(cardNumber);
-                consumer.setFuelCardBalance(consumer.getFuelCardBalance() + value);
-                repository.save(consumer);
-            }
-        }
-
-         */
     }
 
     @ResponseBody
-    @RequestMapping(value = "/buy", method = RequestMethod.GET)
+    @RequestMapping(value = "/buy", method = RequestMethod.PUT)
     public void buy(int establishmentType, String establishmentName, int cardNumber, String productDescription, double value) {
 
         consumerService.buy(establishmentType,establishmentName, cardNumber, productDescription, value);
 
-      //  Consumer consumer = null;
         /* O valores só podem ser debitados dos cartões com os tipos correspondentes ao tipo do estabelecimento da compra.
         *  Exemplo: Se a compra é em um estabelecimeto de Alimentação(food) então o valor só pode ser debitado do cartão e alimentação
         *
@@ -105,35 +82,7 @@ public class ConsumerController {
         * 2 - Farmácia (DrugStore)
         * 3 - Posto de combustivel (Fuel)
         */
-  /*
-        if (establishmentType == 1) {
-            // Para compras no cartão de alimentação o cliente recebe um desconto de 10%
-            Double cashback  = (value / 100) * 10;
-            value = value - cashback;
 
-            consumer = repository.findByFoodCardNumber(cardNumber);
-            consumer.setFoodCardBalance(consumer.getFoodCardBalance() - value);
-            repository.save(consumer);
-
-        }else if(establishmentType == 2) {
-            consumer = repository.findByDrugstoreNumber(cardNumber);
-            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() - value);
-            repository.save(consumer);
-
-        } else {
-            // Nas compras com o cartão de combustivel existe um acrescimo de 35%;
-            Double tax  = (value / 100) * 35;
-            value = value + tax;
-
-            consumer = repository.findByFuelCardNumber(cardNumber);
-            consumer.setFuelCardBalance(consumer.getFuelCardBalance() - value);
-            repository.save(consumer);
-        }
-
-        Extract extract = new Extract(establishmentName, productDescription, new Date(), cardNumber, value);
-        extractRepository.save(extract);
-
-   */
     }
 
 }
--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
@@ -42,16 +42,7 @@ public class Consumer {
     @JsonManagedReference
     @OneToMany(mappedBy = "consumer", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     List<Card> cards;
-    /*
-    int foodCardNumber;
-    double foodCardBalance;
 
-    int fuelCardNumber;
-    double fuelCardBalance;
-
-    int drugstoreNumber;
-    double drugstoreCardBalance;
-     */
 
     @Override
     public boolean equals(Object o) {
@@ -64,10 +55,6 @@ public class Consumer {
                 && phoneNumber == consumer.phoneNumber
                 && number == consumer.number
                 && portalCode == consumer.portalCode
-            //    && foodCardNumber == consumer.foodCardNumber
-           //     && Double.compare(consumer.foodCardBalance, foodCardBalance) == 0
-          //      && fuelCardNumber == consumer.fuelCardNumber && Double.compare(consumer.fuelCardBalance, fuelCardBalance) == 0
-          //      && drugstoreNumber == consumer.drugstoreNumber && Double.compare(consumer.drugstoreCardBalance, drugstoreCardBalance) == 0
                 && Objects.equals(id, consumer.id) && Objects.equals(name, consumer.name) && Objects.equals(birthDate, consumer.birthDate)
                 && Objects.equals(email, consumer.email) && Objects.equals(street, consumer.street) && Objects.equals(city, consumer.city)
                 && Objects.equals(country, consumer.country);
