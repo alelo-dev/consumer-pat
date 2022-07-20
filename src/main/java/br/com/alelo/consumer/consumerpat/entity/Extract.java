c693856344e00083c0a26fd4bd6326cbe7f49d3a
Diogo Ferreira <diogo.ferreira@mercadolivre.com>
2022-07-20 00:25:02 -0300

    fixed annotation

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Extract.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Extract.java
@@ -3,24 +3,27 @@ package br.com.alelo.consumer.consumerpat.entity;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 
 @Data
 @Entity
 @Builder
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class Extract {
 
     @Id
-    int id;
-    int establishmentNameId;
-    String establishmentName;
-    String productDescription;
-    LocalDateTime dateBuy;
-    int cardNumber;
-    double value;
+    private final int id = 0;
+    private int establishmentNameId;
+    private String establishmentName;
+    private String productDescription;
+    private LocalDateTime dateBuy;
+    private int cardNumber;
+    private double value;
 
 }
