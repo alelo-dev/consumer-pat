a54f453a48ecb28a791b126c7191837c1fd96b6b
Diogo Ferreira <diogo.ferreira@mercadolivre.com>
2022-07-20 02:07:22 -0300

    improvement to update consumer

--- src/main/java/br/com/alelo/consumer/consumerpat/usecase/UpdateConsumerUsecase.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/usecase/UpdateConsumerUsecase.java
@@ -1,7 +1,9 @@
 package br.com.alelo.consumer.consumerpat.usecase;
 
+import br.com.alelo.consumer.consumerpat.controller.model.ConsumerRequest;
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,35 @@ public class UpdateConsumerUsecase {
 
     private final ConsumerRepository repository;
 
-    public void execute(Consumer consumer) {
-        repository.save(consumer);
+    public void execute(ConsumerRequest consumerRequest, Integer id) {
+        Optional<Consumer> consumerOpt = repository.findById(id);
+
+        if (consumerOpt.isPresent()) {
+            repository.save(buildUpdateConsumer(consumerOpt.get(), consumerRequest));
+        }
+    }
+
+    private Consumer buildUpdateConsumer(Consumer consumer, ConsumerRequest consumerRequest) {
+        return Consumer.builder()
+            .id(consumer.getId())
+            .name(consumerRequest.getName())
+            .documentNumber(consumerRequest.getDocumentNumber())
+            .birthDate(consumerRequest.getBirthDate())
+            .mobilePhoneNumber(consumerRequest.getMobilePhoneNumber())
+            .residencePhoneNumber(consumerRequest.getResidencePhoneNumber())
+            .phoneNumber(consumerRequest.getPhoneNumber())
+            .email(consumerRequest.getEmail())
+            .street(consumerRequest.getStreet())
+            .number(consumerRequest.getNumber())
+            .city(consumerRequest.getCity())
+            .country(consumerRequest.getCountry())
+            .portalCode(consumerRequest.getPortalCode())
+            .foodCardNumber(consumer.getFoodCardNumber())
+            .foodCardBalance(consumer.getFoodCardBalance())
+            .fuelCardNumber(consumer.getFuelCardNumber())
+            .fuelCardBalance(consumer.getFuelCardBalance())
+            .drugstoreNumber(consumer.getDrugstoreNumber())
+            .drugstoreCardBalance(consumer.getDrugstoreCardBalance())
+            .build();
     }
 }
