f7552e7f2edee4ed3defca6998ce2bb8e0427a91
Diogo Ferreira <diogo.ferreira@mercadolivre.com>
2022-07-19 21:55:23 -0300

    Added method to execute the only reposability of this class

--- src/main/java/br/com/alelo/consumer/consumerpat/usecase/GetAllConsumerUsecase.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/usecase/GetAllConsumerUsecase.java
@@ -1,6 +1,8 @@
 package br.com.alelo.consumer.consumerpat.usecase;
 
+import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Component;
 public class GetAllConsumerUsecase {
     private final ConsumerRepository repository;
 
+    public List<Consumer> execute() {
+        return repository.getAllConsumersList();
+    }
 }
