749d2f4bf60710d6ee01bd9c99f97b1b238df50d
antolioli <108047750+antolioli@users.noreply.github.com>
2023-01-02 12:51:51 -0300

    Usando operador instanceof na linha 52 Consumer.java
    Operador instanceof para testar se o objeto e compara os tipos.
    if (o == null || !(o instanceof Consumer))
    if (o == null || getClass() != o.getClass())
    OBS: 
    Baixei os arquivos e rodei Spring Tool Suite 4 Versão 4.15.3, fiz alguns ajustes para rodar na versão que uso.
    O lombok depara com a annotation @Data, e tenta implementar método equals, a JVM esta
    reclamando porque o método equals existem na classe. Então removi o @Data e usei as anotações @Getter e @Setter.

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
@@ -49,7 +49,7 @@ public class Consumer {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Consumer)) return false;
         Consumer consumer = (Consumer) o;
         return documentNumber == consumer.documentNumber
                 && mobilePhoneNumber == consumer.mobilePhoneNumber
