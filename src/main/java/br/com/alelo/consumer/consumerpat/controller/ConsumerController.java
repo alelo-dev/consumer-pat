9720bf907f17565167a9f28dce12aece939c09e2
iytzpronet <iytzpronet@gmail.com>
2023-01-20 17:43:01 -0300

    projeto concluido

--- src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerController.java
@@ -6,114 +6,114 @@ import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
 import br.com.alelo.consumer.consumerpat.respository.ExtractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 
-
-@Controller
+@RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
 
-    @Autowired
-    ConsumerRepository repository;
+	@Autowired
+	ConsumerRepository repository;
 
-    @Autowired
-    ExtractRepository extractRepository;
+	@Autowired
+	ExtractRepository extractRepository;
 
+	/* Deve listar todos os clientes (cerca de 500) */
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	@RequestMapping(value = "/consumerList", method = RequestMethod.GET)
+	public List<Consumer> listAllConsumers() {
+		return repository.getAllConsumersList();
+	}
 
-    /* Deve listar todos os clientes (cerca de 500) */
-    @ResponseBody
-    @ResponseStatus(code = HttpStatus.OK)
-    @RequestMapping(value = "/consumerList", method = RequestMethod.GET)
-    public List<Consumer> listAllConsumers() {
-        return repository.getAllConsumersList();
-    }
+	/* Cadastrar novos clientes */
+	@RequestMapping(value = "/createConsumer", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createConsumer(@RequestBody Consumer consumer) {
+		repository.save(consumer);
+	}
 
+	// Não deve ser possível alterar o saldo do cartão
+	@RequestMapping(value = "/updateConsumer/{id}", method = RequestMethod.PUT)
+	public void updateConsumer(@RequestBody Consumer consumer, @PathVariable Integer id) {
 
-    /* Cadastrar novos clientes */
-    @RequestMapping(value = "/createConsumer", method = RequestMethod.POST)
-    public void createConsumer(@RequestBody Consumer consumer) {
-        repository.save(consumer);
-    }
+		repository.save(consumer);
 
-    // Não deve ser possível alterar o saldo do cartão
-    @RequestMapping(value = "/updateConsumer", method = RequestMethod.POST)
-    public void updateConsumer(@RequestBody Consumer consumer) {
-        repository.save(consumer);
-    }
+	}
 
+	/*
+	  Deve creditar(adicionar) um valor(value) em um no cartão. Para isso ele
+	  precisa indenficar qual o cartão correto a ser recarregado, para isso deve
+	  usar o número do cartão(cardNumber) fornecido.
+	*/
+	@RequestMapping(value = "/setcardbalance/{cardNumber}/{value}", method = RequestMethod.PUT)
+	public void setBalance(@RequestBody @PathVariable int cardNumber, @PathVariable double value) {
+		Consumer consumer = null;
+		consumer = repository.findByDrugstoreNumber(cardNumber);
 
-    /*
-     * Deve creditar(adicionar) um valor(value) em um no cartão.
-     * Para isso ele precisa indenficar qual o cartão correto a ser recarregado,
-     * para isso deve usar o número do cartão(cardNumber) fornecido.
-     */
-    @RequestMapping(value = "/setcardbalance", method = RequestMethod.GET)
-    public void setBalance(int cardNumber, double value) {
-        Consumer consumer = null;
-        consumer = repository.findByDrugstoreNumber(cardNumber);
+		if (consumer != null) {
+			// é cartão de farmácia
+			consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() + value);
+			repository.save(consumer);
+		} else {
+			consumer = repository.findByFoodCardNumber(cardNumber);
+			if (consumer != null) {
+				// é cartão de refeição
+				consumer.setFoodCardBalance(consumer.getFoodCardBalance() + value);
+				repository.save(consumer);
+			} else {
+				// É cartão de combustivel
+				consumer = repository.findByFuelCardNumber(cardNumber);
+				consumer.setFuelCardBalance(consumer.getFuelCardBalance() + value);
+				repository.save(consumer);
+			}
+		}
+	}
 
-        if(consumer != null) {
-            // é cartão de farmácia
-            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() + value);
-            repository.save(consumer);
-        } else {
-            consumer = repository.findByFoodCardNumber(cardNumber);
-            if(consumer != null) {
-                // é cartão de refeição
-                consumer.setFoodCardBalance(consumer.getFoodCardBalance() + value);
-                repository.save(consumer);
-            } else {
-                // É cartão de combustivel
-                consumer = repository.findByFuelCardNumber(cardNumber);
-                consumer.setFuelCardBalance(consumer.getFuelCardBalance() + value);
-                repository.save(consumer);
-            }
-        }
-    }
+	@ResponseBody
+	@RequestMapping(value = "/buy", method = RequestMethod.GET)
+	public void buy(int establishmentType, String establishmentName, int cardNumber, String productDescription,
+			double value) {
+		Consumer consumer = null;
+		/*
+		 * O valores só podem ser debitados dos cartões com os tipos correspondentes ao
+		 * tipo do estabelecimento da compra. Exemplo: Se a compra é em um
+		 * estabelecimeto de Alimentação(food) então o valor só pode ser debitado do
+		 * cartão e alimentação
+		 *
+		 * Tipos de estabelcimentos 1 - Alimentação (food) 2 - Farmácia (DrugStore) 3 -
+		 * Posto de combustivel (Fuel)
+		 */
 
-    @ResponseBody
-    @RequestMapping(value = "/buy", method = RequestMethod.GET)
-    public void buy(int establishmentType, String establishmentName, int cardNumber, String productDescription, double value) {
-        Consumer consumer = null;
-        /* O valores só podem ser debitados dos cartões com os tipos correspondentes ao tipo do estabelecimento da compra.
-        *  Exemplo: Se a compra é em um estabelecimeto de Alimentação(food) então o valor só pode ser debitado do cartão e alimentação
-        *
-        * Tipos de estabelcimentos
-        * 1 - Alimentação (food)
-        * 2 - Farmácia (DrugStore)
-        * 3 - Posto de combustivel (Fuel)
-        */
+		if (establishmentType == 1) {
+			// Para compras no cartão de alimentação o cliente recebe um desconto de 10%
+			Double cashback = (value / 100) * 10;
+			value = value - cashback;
 
-        if (establishmentType == 1) {
-            // Para compras no cartão de alimentação o cliente recebe um desconto de 10%
-            Double cashback  = (value / 100) * 10;
-            value = value - cashback;
+			consumer = repository.findByFoodCardNumber(cardNumber);
+			consumer.setFoodCardBalance(consumer.getFoodCardBalance() - value);
+			repository.save(consumer);
 
-            consumer = repository.findByFoodCardNumber(cardNumber);
-            consumer.setFoodCardBalance(consumer.getFoodCardBalance() - value);
-            repository.save(consumer);
+		} else if (establishmentType == 2) {
+			consumer = repository.findByDrugstoreNumber(cardNumber);
+			consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() - value);
+			repository.save(consumer);
 
-        }else if(establishmentType == 2) {
-            consumer = repository.findByDrugstoreNumber(cardNumber);
-            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() - value);
-            repository.save(consumer);
+		} else {
+			// Nas compras com o cartão de combustivel existe um acrescimo de 35%;
+			Double tax = (value / 100) * 35;
+			value = value + tax;
 
-        } else {
-            // Nas compras com o cartão de combustivel existe um acrescimo de 35%;
-            Double tax  = (value / 100) * 35;
-            value = value + tax;
+			consumer = repository.findByFuelCardNumber(cardNumber);
+			consumer.setFuelCardBalance(consumer.getFuelCardBalance() - value);
+			repository.save(consumer);
+		}
 
-            consumer = repository.findByFuelCardNumber(cardNumber);
-            consumer.setFuelCardBalance(consumer.getFuelCardBalance() - value);
-            repository.save(consumer);
-        }
-
-        Extract extract = new Extract(establishmentName, productDescription, new Date(), cardNumber, value);
-        extractRepository.save(extract);
-    }
+		Extract extract = new Extract(establishmentName, productDescription, new Date(), cardNumber, value);
+		extractRepository.save(extract);
+	}
 
 }
--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Extract.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Extract.java
@@ -3,24 +3,26 @@ package br.com.alelo.consumer.consumerpat.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
-
 
 @Data
 @Entity
 public class Extract {
 
     @Id
-    int id;
-    int establishmentNameId;
-    String establishmentName;
-    String productDescription;
-    Date dateBuy;
-    int cardNumber;
-    double value;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int establishmentNameId;
+    private String establishmentName;
+    private String productDescription;
+    private Date dateBuy;
+    private int cardNumber;
+    private double value;
 
-
+    /*
     public Extract(int id, int establishmentNameId, String establishmentName, String productDescription, Date dateBuy, int cardNumber, double value) {
         this.id = id;
         this.establishmentNameId = establishmentNameId;
@@ -30,20 +32,22 @@ public class Extract {
         this.cardNumber = cardNumber;
         this.value = value;
     }
-
+    
+    
     public Extract( String productDescription, Date dateBuy, int cardNumber, double value) {
         this.productDescription = productDescription;
         this.dateBuy = dateBuy;
         this.cardNumber = cardNumber;
         this.value = value;
     }
-
+    */
+  
     public Extract(String establishmentName, String productDescription, Date dateBuy, int cardNumber, double value) {
-        this.establishmentNameId = establishmentNameId;
         this.establishmentName = establishmentName;
         this.productDescription = productDescription;
         this.dateBuy = dateBuy;
         this.cardNumber = cardNumber;
         this.value = value;
     }
+    
 }
