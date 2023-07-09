d1afe32bc2d2f2bca73beefa4a46b59b310085f2
Angelica Leite <angelicalleite@gmail.com>
2023-07-08 21:16:54 -0300

    Adição da versão para controller

--- src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerV1Controller.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/controller/ConsumerV1Controller.java
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @AllArgsConstructor
+
+@RestController
 @RequestMapping("/v1/consumer")
 public class ConsumerV1Controller {
 
