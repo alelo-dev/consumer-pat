5bb50683f145c0f77dea8c5dcbafa2d317bafa2c
Erik Santana Marques dos Santos <erik.marques@mosaico.com.br>
2023-07-18 12:27:44 -0300

    descriptive names, readable query

--- src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
@@ -3,21 +3,21 @@ package br.com.alelo.consumer.consumerpat.respository;
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
+    @Query("select c from Consumer c")
+    List<Consumer> getAllConsumers();
 
-    @Query(nativeQuery = true, value = "select * from Consumer")
-    List<Consumer> getAllConsumersList();
+    @Query("select c from Consumer c where c.foodCardNumber = :cardNumber")
+    Consumer findByFoodCardNumber(@Param("cardNumber") int cardNumber);
 
-    @Query(nativeQuery = true, value = "select * from Consumer where FOOD_CARD_NUMBER = ? ")
-    Consumer findByFoodCardNumber(int cardNumber);
+    @Query("select c from Consumer c where c.fuelCardNumber = :cardNumber")
+    Consumer findByFuelCardNumber(@Param("cardNumber") int cardNumber);
 
-    @Query(nativeQuery = true, value = "select * from Consumer where FUEL_CARD_NUMBER = ? ")
-    Consumer findByFuelCardNumber(int cardNumber);
-
-    @Query(nativeQuery = true, value = "select * from Consumer where DRUGSTORE_NUMBER = ? ")
-    Consumer findByDrugstoreNumber(int cardNumber);
+    @Query("select c from Consumer c where c.drugstoreNumber = :cardNumber")
+    Consumer findByDrugstoreNumber(@Param("cardNumber") int cardNumber);
 
 }
