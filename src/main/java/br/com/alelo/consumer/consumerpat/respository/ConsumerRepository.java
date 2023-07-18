759f97cf3ca3abfba99c7aa6a6f4479efa2c796d
Erik Santana Marques dos Santos <erik.marques@mosaico.com.br>
2023-07-18 15:05:39 -0300

    using derived query methods

--- src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/respository/ConsumerRepository.java
@@ -2,22 +2,14 @@ package br.com.alelo.consumer.consumerpat.respository;
 
 import br.com.alelo.consumer.consumerpat.entity.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
-    @Query("select c from Consumer c")
-    List<Consumer> getAllConsumers();
-
-    @Query("select c from Consumer c where c.foodCardNumber = :cardNumber")
     Optional<Consumer> findByFoodCardNumber(@Param("cardNumber") int cardNumber);
 
-    @Query("select c from Consumer c where c.fuelCardNumber = :cardNumber")
     Optional<Consumer> findByFuelCardNumber(@Param("cardNumber") int cardNumber);
 
-    @Query("select c from Consumer c where c.drugstoreCardNumber = :cardNumber")
-    Optional<Consumer> findByDrugstoreNumber(@Param("cardNumber") int cardNumber);
+    Optional<Consumer> findByDrugstoreCardNumber(@Param("cardNumber") int cardNumber);
 }
