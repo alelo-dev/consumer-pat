ecbf4ceb22bb6fc40633e634f3cdafea48903fad
Júlio César <juliocps.br@gmail.com>
2023-10-23 16:36:43 -0300

    Camada de Service
    Criando uma camada de serviço para evitar acesso direto ao repositorio além de organizar um ponto de acesso com as funcionalidades e suas regras implementadas.

---
+++ src/main/java/br/com/alelo/consumer/consumerpat/service/BaseService.java
@@ -0,0 +1,14 @@
+package br.com.alelo.consumer.consumerpat.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Classe de reuso para o componente de service
+ * @author julio.silva
+ * @date 23/10/2023
+ */
+public class BaseService {
+
+	public final Logger log = LoggerFactory.getLogger(getClass());
+}
---
+++ src/main/java/br/com/alelo/consumer/consumerpat/service/ConsumerService.java
@@ -0,0 +1,132 @@
+package br.com.alelo.consumer.consumerpat.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.alelo.consumer.consumerpat.dto.BuyDTO;
+import br.com.alelo.consumer.consumerpat.entity.Consumer;
+import br.com.alelo.consumer.consumerpat.entity.Extract;
+import br.com.alelo.consumer.consumerpat.respository.ConsumerRepository;
+import br.com.alelo.consumer.consumerpat.respository.ExtractRepository;
+
+/**
+ * Camada para isolar o dominio através dos serviços e tratar a possiveis regras
+ * @author julio.silva
+ * @date 23/10/2023
+ */
+@Service
+public class ConsumerService extends BaseService {
+
+	@Autowired
+    ConsumerRepository repository;
+
+    @Autowired
+    ExtractRepository extractRepository;
+    
+    public List<Consumer> listAllConsumers() {
+        log.info("obtendo todos clientes");
+        var consumers = repository.getAllConsumersList();
+
+        return consumers;
+    }
+    
+    /**
+     * Cadastrar novos clientes
+     * @param consumer
+     */
+    public void createConsumer(@RequestBody Consumer consumer) {
+        repository.save(consumer);
+    }
+    
+    /**
+     * Atualizar cliente, lembrando que não deve ser possível alterar o saldo do cartão
+     * @param consumer
+     */
+    public void updateConsumer(@RequestBody Consumer consumer) {
+        repository.save(consumer);
+    }
+    
+    /**
+     * Credito de valor no cartão
+     * cardNumber: número do cartão
+     * value: valor a ser creditado (adicionado ao saldo)
+     */
+    public void setBalance(int cardNumber, double value) {
+        Consumer consumer = null;
+        consumer = repository.findByDrugstoreNumber(cardNumber);
+
+        if(consumer != null) {
+            // é cartão de farmácia
+            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() + value);
+            repository.save(consumer);
+        } else {
+            consumer = repository.findByFoodCardNumber(cardNumber);
+            if(consumer != null) {
+                // é cartão de refeição
+                consumer.setFoodCardBalance(consumer.getFoodCardBalance() + value);
+                repository.save(consumer);
+            } else {
+                // É cartão de combustivel
+                consumer = repository.findByFuelCardNumber(cardNumber);
+                consumer.setFuelCardBalance(consumer.getFuelCardBalance() + value);
+                repository.save(consumer);
+            }
+        }
+    }
+	
+    /**
+     * Débito de valor no cartão (compra)
+     *
+     * establishmentType: tipo do estabelecimento comercial
+     * establishmentName: nome do estabelecimento comercial
+     * cardNumber: número do cartão
+     * productDescription: descrição do produto
+     * value: valor a ser debitado (subtraído)
+     */
+    public void buy(BuyDTO buyDto) {
+        Consumer consumer = null;
+        /* O valor só podem ser debitado do catão com o tipo correspondente ao tipo do estabelecimento da compra.
+
+        *  Exemplo: Se a compra é em um estabelecimeto de Alimentação (food) então o valor só pode ser debitado do cartão alimentação
+        *
+        * Tipos dos estabelcimentos:
+        *    1) Alimentação (Food)
+        *    2) Farmácia (DrugStore)
+        *    3) Posto de combustivel (Fuel)
+        */
+
+        if (establishmentType == 1) {
+            // Para compras no cartão de alimentação o cliente recebe um desconto de 10%
+            Double cashback  = (value / 100) * 10;
+            value = value - cashback;
+
+            consumer = repository.findByFoodCardNumber(cardNumber);
+            consumer.setFoodCardBalance(consumer.getFoodCardBalance() - value);
+            repository.save(consumer);
+
+        }else if(establishmentType == 2) {
+            consumer = repository.findByDrugstoreNumber(cardNumber);
+            consumer.setDrugstoreCardBalance(consumer.getDrugstoreCardBalance() - value);
+            repository.save(consumer);
+
+        } else {
+            // Nas compras com o cartão de combustivel existe um acrescimo de 35%;
+            Double tax  = (value / 100) * 35;
+            value = value + tax;
+
+            consumer = repository.findByFuelCardNumber(cardNumber);
+            consumer.setFuelCardBalance(consumer.getFuelCardBalance() - value);
+            repository.save(consumer);
+        }
+
+        Extract extract = new Extract(establishmentName, productDescription, new Date(), cardNumber, value);
+        extractRepository.save(extract);
+    }
+}
